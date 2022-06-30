038e03b68e55aadef08980f8b828f726ba443b8f
Mezahir  Rajabov <mezahir.rajabov.wesii@gmail.com>
2022-06-30 17:19:29 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, padding: 20, alignItems: 'center'}}>
+      <Text>React Native Wireguard App</Text>
+    </View>
+  );
+};
+
+export default App;
